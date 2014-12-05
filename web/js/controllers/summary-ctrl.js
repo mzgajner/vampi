0661404820220887884da96ee74209c8c095e664
mzgajner <mzgajner@astina.ch>
2014-12-05 13:43:48 +0100

    Add some design changes, change background.

--- web/js/controllers/summary-ctrl.js
+++ web/js/controllers/summary-ctrl.js
@@ -2,6 +2,13 @@
     'use strict';
 
     function SummaryCtrl($location, $route, $scope) {
+        $scope.possibleResults = [
+            'You were faster than most people.',
+            'That was the best time for this word!',
+            'Your result is above average.',
+        ];
+        $scope.randomResult = Math.floor(Math.random() * $scope.possibleResults.length);
+
         $scope.quit = function() {
             $location.path('/main-menu');
         };
